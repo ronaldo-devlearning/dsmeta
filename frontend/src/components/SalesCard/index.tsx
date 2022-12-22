eeb1abe110bd7caf16fff7bd306eb6c0398df731
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-12-22 17:42:21 -0300

    DatePicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,5 +1,11 @@
+import 'react-datepicker/dist/react-datepicker.css'
 import NotificationButton from '../NotificationButton'
 import './style.css'
+
+import DatePicker, { registerLocale } from "react-datepicker";
+import ptBR from 'date-fns/locale/pt-BR';
+
+registerLocale('pt-br', ptBR)
 
 function SalesCard() {
     return (
@@ -7,10 +13,22 @@ function SalesCard() {
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        locale="pt-br"
+                        selected={new Date()}
+                        onChange={(date: Date) => {}}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        locale="pt-br" 
+                        selected={new Date()}
+                        onChange={(date: Date) => {}}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
             </div>
             <div>
